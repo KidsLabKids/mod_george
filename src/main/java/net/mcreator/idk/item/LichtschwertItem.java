5f76d9576977cbfd1dddf1680685f17778310ce4
kidslabkids <kidslab@KidsLabStation>
2021-12-23 16:20:22 +0100

    erste verison

---
+++ src/main/java/net/mcreator/idk/item/LichtschwertItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.idk.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.idk.IdkModElements;
+
+@IdkModElements.ModElement.Tag
+public class LichtschwertItem extends IdkModElements.ModElement {
+	@ObjectHolder("idk:lichtschwert")
+	public static final Item block = null;
+	public LichtschwertItem(IdkModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("lichtschwert"));
+	}
+}
